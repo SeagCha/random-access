4a337c94672404997be1b5f59cfc88b97be60dc7
Eugene Chayka <89916248+SeagCha@users.noreply.github.com>
2023-12-17 21:50:53 +0300

    Update tree.cpp

--- random-access-iterator/tree.cpp
+++ random-access-iterator/tree.cpp
@@ -1,13 +1,34 @@
 #include "tree.hpp"
 
 tree::iterator operator+(tree::iterator const &self, ptrdiff_t diff) {
-    return self;
+    auto tmp = self;
+    ptrdiff_t i = 0;
+
+    while (i > diff) {
+        tmp--;
+        i--;
+    }
+
+    while (i < diff) {
+        tmp++;
+        i++;
+    }
+
+    return tmp;
 }
 
 ptrdiff_t operator-(tree::iterator const &self, tree::iterator other) {
-    return 0;
+    return self.index - other.index;
 }
 
-auto tree::begin() const -> iterator { return iterator{root.get()}; }
+auto tree::begin() const -> iterator {
+    auto ptr = root.get();
+    while (ptr->left) {
+        ptr = ptr->left.get();
+    }
+    return {ptr, 0};
+}
 
-auto tree::end() const -> iterator { return iterator{root.get()}; }
+auto tree::end() const -> iterator {
+    return iterator{root.get(), root->subtree_size};
+}
